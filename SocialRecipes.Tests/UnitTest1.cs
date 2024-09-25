f61058f3f35ae0e26e55cf455d26c47dddf91674
JrKoolen <81234655+JrKoolen@users.noreply.github.com>
2024-09-25 15:44:17 +0200

    added a diffrent kind of unit tests with a cs.proj

---
+++ SocialRecipes.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace SocialRecipes.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AlwaysPass()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+
+}
