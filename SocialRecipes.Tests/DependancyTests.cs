bd5a86ed54a23aa4c4234e11545e58a1d7d83ba8
JrKoolen <81234655+JrKoolen@users.noreply.github.com>
2024-10-09 22:59:36 +0200

    Updated unit tests now test relevant parts of the build

---
+++ SocialRecipes.Tests/DependancyTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SocialRecipes.DAL.Repositories;
+using SocialRecipes.Domain.IRepositories; // Adjust namespaces as necessary
+using SocialRecipes.Domain.IServices;
+using SocialRecipes.Services.Services; // Adjust namespaces as necessary
+
+namespace DependancyTests.Tests
+{
+    [TestClass]
+    public class DependencyInjectionTests
+    {
+        private ServiceProvider _serviceProvider;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            string conn = "default";
+            var serviceCollection = new ServiceCollection();
+
+            // Register the repository
+            serviceCollection.AddScoped<IUserRepository, UserRepository>(provider =>
+            {
+                var connectionString = conn;
+                return new UserRepository(connectionString);
+            });
+
+            // Register the service
+            serviceCollection.AddScoped<IUserService, UserService>();
+
+            // Build the service provider
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+        }
+
+        [TestMethod]
+        public void UserService_Should_Be_Registered()
+        {
+            // Act
+            var userService = _serviceProvider.GetService<IUserService>();
+
+            // Assert
+            Assert.IsNotNull(userService);
+            Assert.IsInstanceOfType(userService, typeof(UserService));
+        }
+    }
+}
