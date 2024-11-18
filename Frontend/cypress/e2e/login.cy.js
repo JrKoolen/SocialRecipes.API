4c916f3df8aabec0eab5f6f819b5ceb209800ca0
JrKoolen <81234655+JrKoolen@users.noreply.github.com>
2024-11-18 21:14:49 +0100

    test

--- Frontend/cypress/e2e/login.cy.js
+++ Frontend/cypress/e2e/login.cy.js
@@ -1,49 +1,39 @@
-import { loginFields, testData } from '../support/testData';
-
 describe('Login Page Tests', () => {
-  const baseUrl = 'http://localhost:3000/login';
-
-  function fillLoginForm({ username, password }) {
-    if (username) cy.get(loginFields.username).type(username);
-    if (password) cy.get(loginFields.password).type(password);
-  }
-
   it('should load the login page successfully', () => {
-    cy.visit(baseUrl);
-    cy.contains('Login').should('be.visible');
+    cy.visit('http://localhost:3000/login');
+    cy.contains('Login').should('be.visible'); 
   });
 
   it('should show an error on submitting empty login form', () => {
-    cy.visit(baseUrl);
-    cy.get(loginFields.submitButton).click();
-
-    cy.get(loginFields.username).should('have.attr', 'required');
-    cy.get(loginFields.username).then(($input) => {
+    cy.visit('http://localhost:3000/login');
+    cy.get('button[type="submit"]').click();
+  
+    cy.get('input[name="username"]').should('have.attr', 'required');
+    cy.get('input[name="username"]').then(($input) => {
       expect($input[0].checkValidity()).to.be.false;
     });
-
-    cy.get(loginFields.password).should('have.attr', 'required');
-    cy.get(loginFields.password).then(($input) => {
+  
+    cy.get('input[name="password"]').should('have.attr', 'required');
+    cy.get('input[name="password"]').then(($input) => {
       expect($input[0].checkValidity()).to.be.false;
     });
   });
 
   it('should login successfully with valid credentials', () => {
-    cy.visit(baseUrl);
+    cy.visit('http://localhost:3000/login');
+    cy.get('input[name="username"]').type('testuser'); 
+    cy.get('input[name="password"]').type('Password123');
+    cy.get('button[type="submit"]').click();
 
-    const validData = testData.validSubmission.data; 
-    fillLoginForm({ username: validData.username, password: validData.password });
-
-    cy.get(loginFields.submitButton).click();
-
-    cy.url().should('include', '/user-page');
+    cy.url().should('include', '/user-page'); 
     cy.contains('Welcome').should('be.visible');
   });
 
   it('should show an error with invalid credentials', () => {
-    cy.visit(baseUrl);
-    fillLoginForm({ username: 'invalidUsername', password: 'wrongpassword' });
-    cy.get(loginFields.submitButton).click();
+    cy.visit('http://localhost:3000/login');
+    cy.get('input[name="username"]').type('invalidUsername');
+    cy.get('input[name="password"]').type('wrongpassword');
+    cy.get('button[type="submit"]').click();
 
     cy.contains('Login failed. Please try again.').should('be.visible');
   });
