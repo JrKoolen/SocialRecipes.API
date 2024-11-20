01e8caada54f35ece8afb01cb42496f521c443b0
JrKoolen <81234655+JrKoolen@users.noreply.github.com>
2024-11-20 19:48:18 +0100

    new yml file

--- update-Docker.sh
+++ update-Docker.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
-
+ls -l
 echo "Pulling latest changes from master branch..."
-git pull origin master
+git_output=$(git pull origin master)
+echo "$git_output"
 
-PROJECT_DIR="/path/to/your/project"
-cd $PROJECT_DIR || exit
+if [[ "$git_output" == *"Already up to date."* ]]; then
+    echo "Repository is already up to date. Continuing with Docker restart..."
+else
+    echo "Repository updated. Proceeding with Docker restart..."
+fi
 
 echo "Rebuilding and restarting Docker containers..."
 docker-compose down
 docker-compose up --build -d
 
+
+sleep 30
+
 echo "Checking running containers..."
 docker ps
+
+echo "Press Enter to exit..."
+read -r
