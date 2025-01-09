28ab72f5b4dc23b0a67528c0bcfff303aabcd01a
JrKoolen <81234655+JrKoolen@users.noreply.github.com>
2025-01-09 22:10:56 +0100

    test

--- admin/src/views/AnalyticsPage.vue
+++ admin/src/views/AnalyticsPage.vue
@@ -3,12 +3,12 @@
     <h1 class="dashboard-title">Analytics</h1>
     <div class="dashboard-grid">
       <div class="dashboard-box">
-        <p>Total users</p>
-        <p>{{ totalUsers }}</p>
+        <h2>Total users</h2>
+        <h2>{{ totalUsers }}</h2>
       </div>
       <div class="dashboard-box">
-        <p>Total recipes</p>
-        <p>{{ totalRecipes }}</p>
+        <h2>Total recipes</h2>
+        <h2>{{ totalRecipes }}</h2>
       </div>
     </div>
   </div>
@@ -32,8 +32,8 @@ export default {
           getTotalUsers(),
           getTotalRecipes(),
         ]);
-        this.totalUsers = usersResponse.data.total || 0; 
-        this.totalRecipes = recipesResponse.data.total || 0;
+        this.totalUsers = usersResponse.data;
+        this.totalRecipes = recipesResponse.data;
       } catch (error) {
         console.error("Error fetching analytics data:", error);
       }
@@ -56,13 +56,13 @@ export default {
 .dashboard-grid {
   display: grid;
   grid-template-columns: repeat(2, 1fr);
-  gap: 100px;
+  gap: 20px;
   margin-top: 20px;
 }
 
 .dashboard-box {
   background: #fff;
-  border: 2px solid #007bff;
+  border: 2px solid #000000;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   padding: 40px;
   text-align: center;
@@ -71,5 +71,8 @@ export default {
   border-radius: 8px;
   height: 200px;
   width: 90%;
+  transition: background-color 0.3s ease; 
+  background-color: hsl(134, 61%, 41%); 
+  color: white;
 }
 </style>
