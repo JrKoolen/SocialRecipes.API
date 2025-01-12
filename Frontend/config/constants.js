ad83e6664ba06bde0bf3ad23783d8c5e90cdf17e
JrKoolen <81234655+JrKoolen@users.noreply.github.com>
2025-01-12 02:46:32 +0100

    fixed

--- Frontend/config/constants.js
+++ Frontend/config/constants.js
@@ -1,6 +1,6 @@
 const BASE_URL = process.env.API_BASE_URL || (process.env.NODE_ENV === 'production'
-  ? 'https://socialrecipesapi-e4hjhse0f6cxfgen.westeurope-01.azurewebsites.net/'
-  : 'https://socialrecipesapi-e4hjhse0f6cxfgen.westeurope-01.azurewebsites.net/');
+  ? 'https://socialrecipesapi-e4hjhse0f6cxfgen.westeurope-01.azurewebsites.net/api'
+  : 'https://socialrecipesapi-e4hjhse0f6cxfgen.westeurope-01.azurewebsites.net/api');
 module.exports = {
   AUTH: {
     LOGIN: `${BASE_URL}/Auth/login`,
@@ -36,4 +36,5 @@ module.exports = {
 };
 // @R3x!9Tq#Lz@1pV! 
 //http://socialrecipes-frontend-fcffbc96197724946bd2384afc88861792e721fd.westeurope.azurecontainer.io:3000/recipes
+// "https://socialrecipesapi-e4hjhse0f6cxfgen.westeurope-01.azurewebsites.net/api"
 //http://socialrecipes-api-container:8080/api
